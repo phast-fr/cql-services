fc8925c505ae20a26511780969771a4541f5f8e7
David Ouagne <david.ouagne@phast.fr>
2021-11-12 16:43:55 +0100

    chore(dependencies): update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "fr.phast"
-version = "0.0.12-SNAPSHOT"
+version = "0.0.13-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
@@ -31,7 +31,7 @@ dependencies {
     implementation("org.opencds.cqf.cql:engine:1.5.2")
 
     implementation("fr.phast:phast-fhir-kt:0.0.10-SNAPSHOT")
-    implementation("fr.phast:cql-engine-fhir:0.0.8-SNAPSHOT")
+    implementation("fr.phast:cql-engine-fhir:0.0.9-SNAPSHOT")
 }
 
 tasks.withType<KotlinCompile> {
